fba47d99c4d79ca91d282888fc1bc9d3b9c81f4f
Jean-Baptiste Trystram <blitzkrieg263@gmail.com>
2016-10-18 13:54:41 +0200

    avoid Split brain and allow other replicas to be elected

--- mongo_cluster/setup/mongosetup.sh
+++ mongo_cluster/setup/mongosetup.sh
@@ -3,18 +3,22 @@
 MONGODB1=$(ping -c 1 mongo1 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1)
 MONGODB2=$(ping -c 1 mongo2 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1)
 MONGODB3=$(ping -c 1 mongo3 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1)
+MONGODB4=$(ping -c 1 mongo4 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1)
+
 
 echo "Waiting for startup.."
 until curl --silent http://${MONGODB1}:28017/ 2>&1 | grep -q 'waiting for connections on port'; do
   printf '.'
   sleep 1
 done
+echo ''
 
 echo curl --silent http://${MONGODB1}:28017/serverStatus\?text\=1 2>&1 | grep uptime | head -1
-echo "Started.."
+echo "MongoDB instances are ready.."
 
 sleep 10
 
+echo "Initiating replication."
 echo SETUP.sh time now: $(date +"%T")
 mongo --host ${MONGODB1}:27017 <<EOF
    var cfg = {
@@ -29,12 +33,17 @@ mongo --host ${MONGODB1}:27017 <<EOF
             {
                 "_id": 1,
                 "host": "${MONGODB2}:27017",
-                "priority": 0
+                "priority": 1
             },
             {
                 "_id": 2,
                 "host": "${MONGODB3}:27017",
-                "priority": 0
+                "priority": 1
+            },
+            {
+                "_id": 3,
+                "host": "${MONGODB4}:27017",
+                "priority": 1
             }
         ]
     };
